298c0ea0cea981fe27846036a2edbea3048d7be8
Susana Becerra Pino <susanabp1992@gmail.com>
2023-12-28 21:49:54 -0500

    Adds slash prefixto routes

---
+++ assets/Sales-9628c997.js
@@ -0,0 +1,1 @@
+import{_ as a,L as o,a as t,o as n,i as c,w as l,b as e}from"./index-d45f0998.js";const p={components:{Layout:o}},r=e("main",{id:"sales-page"},[e("h1",null,"Sales"),e("p",null,"This is the Sales page")],-1);function _(i,u,m,d,f,h){const s=t("Layout");return n(),c(s,null,{main:l(()=>[r]),_:1})}const L=a(p,[["render",_]]);export{L as default};

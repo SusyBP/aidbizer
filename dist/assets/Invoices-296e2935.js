8730a1f77095b96219f271a0af757ee20dd1386a
Susana Becerra Pino <susanabp1992@gmail.com>
2023-12-30 23:25:31 -0500

    Updates dist

--- dist/assets/Invoices-296e2935.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,L as n,a as t,o as a,g as c,w as i,b as o}from"./index-120dd710.js";const p={components:{Layout:n}},r=o("main",{id:"invoices-page"},[o("h1",null,"Invoices"),o("p",null,"This is the Invoices page")],-1);function _(l,u,m,d,f,h){const e=t("Layout");return a(),c(e,null,{main:i(()=>[r]),_:1})}const x=s(p,[["render",_]]);export{x as default};

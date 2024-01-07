2616204eedb59ede5996dd81e9ba1e817ce8400c
Susana Becerra Pino <susanabp1992@gmail.com>
2024-01-06 23:13:53 -0500

    Styles Sign Up page social media links

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -5,12 +5,12 @@
             <form class="form-group">
                 <div class="form-group">
                     <label for="user-email" class="text-uppercase fw-bold">Email</label>
-                    <input id="user-email" class="form-control" type="text" v-model="username"
-                        autocomplete="johndue@domail.com">
+                    <input id="user-email" class="form-text-input form-control" type="text" v-model="username"
+                        autocomplete="johndue@domail.com">  
                 </div>
                 <div class="form-group">
                     <label for="user-password" class="text-uppercase fw-bold">Password</label>
-                    <input id="user-password" class="form-control" type="password" v-model="password"
+                    <input id="user-password" class="form-text-input form-control" type="password" v-model="password"
                         autocomplete="current-password">
                 </div>
 
@@ -26,12 +26,18 @@
                 <div class="external-login form-group mt-3">
                     <!-- <div class="text-theme">Login with another account</div> -->
                     <div class="external-login-brands">
-                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 488 512">
-                            <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/></svg></a>
-                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 320 512">   
-                            <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg></a>
-                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 448 512">
-                            <path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/></svg></a>
+                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="25px" width="25px" viewBox="0 0 488 512"  fill="#0c5f5f" >
+                                <path
+                                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+                            </svg></a>
+                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="24px" width="30px" viewBox="0 0 320 512" fill="#0c5f5f">
+                                <path
+                                    d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
+                            </svg></a>
+                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 448 512" fill="#0c5f5f">
+                                <path
+                                    d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" />
+                            </svg></a>
                     </div>
                 </div>
                 <div class="policies form-group mt-3 d-flex flex-wrap justify-content-between">
@@ -140,14 +146,29 @@ input {
     margin-bottom: 1.5rem;
 
 }
-.external-login-brands{
-text-align: center;
-margin-top: 2rem;
-margin-bottom: 3rem;
+
+.external-login-brands {
+    text-align: center;
+    margin-top: 2rem;
+    margin-bottom: 3rem;
 }
 
-svg{
-   color: var(--theme-green);
-   margin-right: 1rem;
+svg {
+    color: var(--theme-green);
+    margin-right: 1rem;
+}
+
+.form-text-input {
+    padding: .5rem;
+}
+
+.form-text-input:focus {
+    border-color: rbg(12,95,95,0.18) !important;
+    box-shadow: 0 0 0 0.25rem var(--theme-green-6) !important;
+}
+
+.form-text-input:focus-visible {
+    border-color: var(--theme-green-6) !important;;
+    /* box-shadow: 0 0 0 0.25rem var(--theme-green-6) !important; */
 }
 </style>

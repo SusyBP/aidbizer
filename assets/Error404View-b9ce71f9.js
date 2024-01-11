0d91bb7bc2eaf235b824d38050761946eda6ca1c
Susana Becerra Pino <susanabp1992@gmail.com>
2024-01-11 07:43:56 -0500

    Form Validaton on Sign Up page

---
+++ assets/Error404View-b9ce71f9.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-6eeaba91.js";const e={};function n(o,t){return" Page not found "}const _=r(e,[["render",n]]);export{_ as default};
---
+++ assets/Orders-d5908fe8.js
@@ -0,0 +1,1 @@
+import{_ as s,L as t,a,o as n,f as r,w as c,b as e}from"./index-6eeaba91.js";const p={components:{Layout:t}},_=e("main",{id:"orders-page"},[e("h1",null,"Orders"),e("p",null,"This is the Orders page")],-1);function d(i,l,u,m,f,h){const o=a("Layout");return n(),r(o,null,{main:c(()=>[_]),_:1})}const L=s(p,[["render",d]]);export{L as default};

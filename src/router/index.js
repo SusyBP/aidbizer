b7883179cf7c8675e57a0252bec84ee4fd6a9b0c
Susana Becerra Pino <susanabp1992@gmail.com>
2024-02-15 22:22:19 -0500

    Adds AssociateToCompany view

--- src/router/index.js
+++ src/router/index.js
@@ -10,104 +10,110 @@ import SignUp from '../views/SignUp.vue'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [    
+  routes: [
     {
       path: '/',
       // name: 'Page',
       component: () => import('../components/MainComponent.vue'),
       children: [
-          {
-              path: '/aidbizer',
-              name: 'Home',
-              component: Home,
-              auth: true
-          },
-          {
-              path: '/aidbizer/debts',
-              name: 'Debts',  
-              component: () => import('../views/Debts.vue'),
-              auth: true
-          },
-          {
-              path: '/aidbizer/products',
-              name: 'Products',
-              component: () => import('../views/ProductsView.vue'),
-              auth: true
-          },
-          {
-            path: '/aidbizer/sales',
-            name: 'Sales',
-            component: () => import('../views/Sales.vue'),
-            auth: true
-          },
-          {
-            path: '/aidbizer/orders',
-            name: 'Orders',
-            component: () => import('../views/Orders.vue'),
-            auth: true
-          },
-          {
-            path: '/aidbizer/expenses',
-            name: 'Expenses',
-            component: () => import('../views/Expenses.vue'),
-            auth: true
-          },
-          {
-            path: '/aidbizer/stock',
-            name: 'Stock',//stock de productos
-            component: () => import('../views/Stock.vue'),
-            auth: true
-          },
-          {
-            path: '/aidbizer/inventory',
-            name: 'Inventory',//stock de materias primas
-            component: () => import('../views/Inventory.vue'),
-            auth: true
-          },
-          {
-            path: '/aidbizer/production-processes',
-            name: 'ProductionProcesses',//stock de materias primas
-            component: () => import('../views/ProductionProcesses.vue'),
-            auth: true
-          },
-          {
-            path: '/aidbizer/invoices',
-            name: 'Invoices',
-            component: () => import('../views/Invoices.vue'),
-            auth: true
-          },
-          {
-            path: '/aidbizer/quote',
-            name: 'Quote',
-            component: () => import('../views/Quote.vue'),
-            auth: true
-          },
-          {
-            path: '/aidbizer/balance',
-            name: 'Balance',
-            component: () => import('../views/Balance.vue'),
-            auth: true
-          },
-          {
-            path: '/aidbizer/contacts',
-            name: 'Contacts',
-            component: () => import('../views/Contacts.vue'),
-            auth: true
-          },
+        {
+          path: '/aidbizer',
+          name: 'Home',
+          component: Home,
+          auth: true
+        },
+        {
+          path: '/aidbizer/associate',
+          name: 'Associate',
+          component: () => import('../views/AssociateCompanyView.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/debts',
+          name: 'Debts',
+          component: () => import('../views/Debts.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/products',
+          name: 'Products',
+          component: () => import('../views/ProductsView.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/sales',
+          name: 'Sales',
+          component: () => import('../views/Sales.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/orders',
+          name: 'Orders',
+          component: () => import('../views/Orders.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/expenses',
+          name: 'Expenses',
+          component: () => import('../views/Expenses.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/stock',
+          name: 'Stock',//stock de productos
+          component: () => import('../views/Stock.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/inventory',
+          name: 'Inventory',//stock de materias primas
+          component: () => import('../views/Inventory.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/production-processes',
+          name: 'ProductionProcesses',//stock de materias primas
+          component: () => import('../views/ProductionProcesses.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/invoices',
+          name: 'Invoices',
+          component: () => import('../views/Invoices.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/quote',
+          name: 'Quote',
+          component: () => import('../views/Quote.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/balance',
+          name: 'Balance',
+          component: () => import('../views/Balance.vue'),
+          auth: true
+        },
+        {
+          path: '/aidbizer/contacts',
+          name: 'Contacts',
+          component: () => import('../views/Contacts.vue'),
+          auth: true
+        },
       ]
-  },
+    },
     {
       path: '/aidbizer/signin',
       name: 'SignIn',
       component: SignIn
-    },  
+    },
     {
       path: '/aidbizer/signup',
       name: 'SignUp',
       component: SignUp
     },
-    
-  
+
+
     {
       path: '/:pathMatch(.*)*',
       name: 'error404',
---
+++ src/views/AssociateCompanyView.vue
@@ -0,0 +1,7 @@
+<template>
+	<main id="page">
+		<h1>Create Company</h1>
+		<p>Create a Compay</p>
+		
+	</main>
+</template>
